6369d474c050b01b586a7a9295d04eb89bf82efd
goldenxuett <ttanpcs@gmail.com>
2021-01-23 20:08:39 -0600

    Create testing program

--- GoCamera.py
+++ GoCamera.py
@@ -1,24 +1,24 @@
 from picamera import PiCamera
 from time import sleep
 import numpy as np
-import sys
 import functools
 import cv2
-sys.path.insert(1, '/home/pi/G.O.A.T-Project/submodules/watchGo')
 import watchGo
 
-class GoCamera:
-    def __init__(self, bs):
-        self.camera = PiCamera()
-        self.board_size = bs
-        camera.start_preview(alpha = 192)
-        self.frame_size = None
+# class GoCamera:
+#     def __init__(self, bs):
+#         self.camera = PiCamera()
+#         self.board_size = bs
+#         camera.start_preview(alpha = 192)
+#         self.frame_size = None
 
-# camera = PiCamera()
-# camera.start_preview()
-# sleep(1)
-# name = "RoyDumb.jpg"
-# camera.capture("/home/pi/G.O.A.T-Project/temp/" + name)
-# camera.stop_preview()
-# camera.close()
-# print("hi")
+camera = PiCamera()
+camera.start_preview()
+sleep(1)
+pre_name = "./resources/preTest.jpg"
+post_name = "./resources/postTest.jpg"
+camera.capture(pre_name)
+a = input("Press a key")
+camera.capture(post_name)
+camera.stop_preview()
+camera.close()

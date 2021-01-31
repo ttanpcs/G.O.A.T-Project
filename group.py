55eeabab3e25e5c3b8308e5271b6876956e0ede6
raymocao <raymondcao2002@gmail.com>
2021-01-30 23:03:32 -0600

    Add basic methods

--- group.py
+++ group.py
@@ -3,8 +3,16 @@ class Group (object):
         self.color_ = color
         self.tile_array_ = []
 
+    def __init__ (self, color, coordinates):
+        self.color_ = color
+        self.tile_array_ = []
+        self.tile_array_.append(coordinates)
+
     def Add_Tile (self, coordinates):
-        self.tile_array_.add(coordinates)
+        self.tile_array_.append(coordinates)
 
     def Get_Tiles(self):
         return tile_array_
+
+    def Get_Size(self):
+        return len(self.tile_array_)

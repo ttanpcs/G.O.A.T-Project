323efedb12cd9105c7fd190fe9becc6a08aa22ed
goldenxuett <ttanpcs@gmail.com>
2021-02-20 00:28:38 -0600

    Update Player class to be abstract

--- player.py
+++ player.py
@@ -1,34 +1,41 @@
 from abc import ABC, abstractmethod
 import random
+import board
+import numpy as np
 import constants, enums
 
 class Player (ABC):
-    def __init__ (self, is_AI):
+    def __init__ (self, is_AI, tile_type):
         self.is_AI_ = is_AI
-        self.possible_moves_ = [[True] * 19] * 19
-        self.captured_pieces_ = 0
+        self.tyle_type = tile_type
 
     def Is_AI (self):
         return self.is_AI_
 
-    def Take_Turn(self):
-        """
-        Potential AI stuff goes here.
-        """
+    @abstractmethod
+    def Take_Turn (self, board):
         pass
 
-    def Take_Random_Turn (self, board):
+class HumanPlayer (Player):
+    def Take_Turn (self, board):
+        print("-----------------------------------------------------")
+        current_input = input("Type \"p\" to pass")
+        if (current_input == "p"):
+            return True, None
+        else:
+            return False, None
+
+class RandomAIPlayer (Player):
+    def Take_Turn (self, board):
         random.seed()
         x = random.randint(0, constants.GO_BOARD_LENGTH)
         y = random.randint(0, constants.GO_BOARD_LENGTH)
-        while (board.Find_Tile(x, y) != enums.TileType.NO_TILE):
+
+        while (board.getPiece(x, y) != enums.TileType.NO_TILE):
             x = random.randint(0, constants.GO_BOARD_LENGTH)
             y = random.randint(0, constants.GO_BOARD_LENGTH)
         
-        coords = (x, y)
-        return coords
-
-        
-
-    def Update_Possible_Moves(self, new_moves):
-        self.possible_moves_ = new_moves
+        will_pass = random.randint(0, 30) # Note That these numbers are chosen randomly
+        if (will_pass == 1):
+            return True, np.array([x, y])
+        return False, np.array([x, y])
